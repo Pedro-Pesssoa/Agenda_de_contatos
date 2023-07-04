0039263d81cd6775f123623c23fdaa24d0a44872
Pedro-Pesssoa <pedropessoacaeira@gmail.com>
2023-07-04 10:17:05 -0300

    configuração adm

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'description',
+    ordering = 'id',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 50
+    list_editable = 'phone',
+    list_display_links = 'id', 'first_name',
+
